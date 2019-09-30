d34c45b0cce5e091ce852c371ec71ec75d0c32f3
Matthew Gaiser <Matthew.Gaiser@CalgaryParking.com>
2019-09-30 14:38:19 -0600

    first commit

---
+++ backend/src/main/java/com/PPM/PPMTool/PpmToolApplication.java
@@ -0,0 +1,13 @@
+package com.PPM.PPMTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApplication.class, args);
+	}
+
+}
